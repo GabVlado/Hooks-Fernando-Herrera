1aa0b039f87beb99dbb537579dc0f41784f95e73
Gabriel Vladimir <gv.valdivieso2309@gmail.com>
2021-02-11 09:13:11 -0500

    Mousemove , AddEventListene, useEffect

---
+++ src/components/02-useEffect/Message.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+
+const Message = () => {
+
+    const [coords, setCoords] = useState({
+        x: 0,
+        y:0
+    })
+
+    const {x,y} = coords;
+
+    useEffect(() => {
+
+        const mouseMove = ( e ) =>{ 
+            const coors = { x: e.x  , y : e.y }
+            setCoords( coors );
+        }
+
+        window.addEventListener('mousemove', mouseMove)
+        
+
+        return () => {
+            window.removeEventListener('mousemove',mouseMove)
+        }
+    }, [])
+
+    return (
+        <div>
+            <h3>Coors</h3>
+            <p> x : {x} y: {y} </p>
+        </div>
+    )
+}
+
+export default Message
