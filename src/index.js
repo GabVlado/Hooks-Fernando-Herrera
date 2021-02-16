f7cf36743c725a91950baf5be6b104cd81cf8aad
Gabriel Vladimir <gv.valdivieso2309@gmail.com>
2021-02-15 20:58:38 -0500

    useReducer intro

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Padre } from './components/07-tarea-memo/Padre';
-//import CallbackHook from './components/06-memo/CallbackHook';
-//import FocusScreen from './components/04-useRef/FocusScreen';
-//import MultipleCustomHooks from './components/03-examples/MultipleCustomHooks';
-//import FormWithCustomHook from './components/02-useEffect/FormWithCustomHook';
-//import SimpleForm from './components/02-useEffect/SimpleForm';
-//import CounterWithCustomHook from './components/01-useState/CounterWithCustomHook';
-//import CounterApp from './components/01-useState/CounterApp';
-//import HooksApp from './HooksApp';
-//import RealExampleRef from './components/04-useRef/RealExampleRef';
-//import Layout from './components/05-useLayoutEffect/Layout';
-//import Memorize from './components/06-memo/Memorize';
-//import { MemoHook } from './components/06-memo/MemoHook';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { Padre } from './components/07-tarea-memo/Padre';
+// //import CallbackHook from './components/06-memo/CallbackHook';
+// //import FocusScreen from './components/04-useRef/FocusScreen';
+// //import MultipleCustomHooks from './components/03-examples/MultipleCustomHooks';
+// //import FormWithCustomHook from './components/02-useEffect/FormWithCustomHook';
+// //import SimpleForm from './components/02-useEffect/SimpleForm';
+// //import CounterWithCustomHook from './components/01-useState/CounterWithCustomHook';
+// //import CounterApp from './components/01-useState/CounterApp';
+// //import HooksApp from './HooksApp';
+// //import RealExampleRef from './components/04-useRef/RealExampleRef';
+// //import Layout from './components/05-useLayoutEffect/Layout';
+// //import Memorize from './components/06-memo/Memorize';
+// //import { MemoHook } from './components/06-memo/MemoHook';
 
-ReactDOM.render(
-    <Padre />,
-  document.getElementById('root')
-);
+// ReactDOM.render(
+//     <Padre />,
+//   document.getElementById('root')
+// );
+
+import './components/08-useReducer/intro-reducer';
